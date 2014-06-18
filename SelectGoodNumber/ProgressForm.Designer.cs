ca83bf70c135cf005da5b01ad71a6f376c7143fd
dvwei <davidwudv@gmail.com>
2014-06-18 11:57:42 +0800

    version 1.1

---
+++ SelectGoodNumber/ProgressForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace SelectGoodNumber
+{
+    partial class ProgressForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "数据导入中.......";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 29);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(259, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // ProgressForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 61);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "ProgressForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "请稍后";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ SelectGoodNumber/ProgressForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SelectGoodNumber
+{
+    public partial class ProgressForm : Form
+    {
+        private Thread _runThread;
+        private delegate void SetProgressCallBack(int value);
+        public ProgressForm()
+        {
+            InitializeComponent();
+        }
+
+        //public ProgressBar CurrentProgressBar
+        //{
+        //    get { return this.progressBar1; }
+        //}
+
+        public void SetProgress(int value)
+        {
+            if(this.progressBar1.InvokeRequired)
+            {
+                SetProgressCallBack call = new SetProgressCallBack(SetProgressValue);
+                this.progressBar1.Invoke(call, new object[] {value});
+            }
+        }
+
+        private void SetProgressValue(int value)
+        {
+            this.progressBar1.Value = value;
+        }
+
+        public void Start()
+        {
+            _runThread = new Thread(_start);
+            _runThread.Start();
+        }
+
+        public void Stop()
+        {
+            if (_runThread != null)
+                _runThread.Abort();
+        }
+
+        private void _start(object obj)
+        {
+            try
+            {
+                ShowDialog();
+            }
+            catch (ThreadAbortException)
+            {
+                this.Close();
+                this.Dispose();
+            }
+            catch (Exception 
+#if DEBUG
+                ex
+#endif
+                )
+            {
+#if DEBUG
+                MessageBox.Show(ex.Message);
+#endif
+            }
+        }
+    }
+}
